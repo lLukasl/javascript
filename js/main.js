25c0722b298b3cc206ba9a06c46182d0518a6e91
lLukasl <llukasdiasl@hotmail.com>
2020-01-15 23:19:11 -0300

    primeiras alteracoes de teste em js puro

---
+++ js/main.js
@@ -0,0 +1,47 @@
+var d = new Date();
+alert(d);
+alert(d.getFullYear());
+
+// var count;
+// for(count=0; count <=5; count++){
+//     alert(count);
+// }
+
+// var count = 0;
+// while(count < 5){
+//     console.log(count);
+//     alert(count);
+//     count ++;
+// };
+
+
+// var idade = prompt("Digite sua idade");
+// if (idade >= 18){
+//     alert("Entre");
+// }else{
+//     alert("VSF");
+// }
+
+/*var frutas = [{nome: "lucas", fruta: "maçã"}, {nome: "ana", fruta: "uva"}];
+console.log(frutas);
+alert(frutas[1].fruta);
+*/
+
+
+
+//var nome = "Fudencio da Silva";
+//var idade = 30;
+//var idade2 = 10;
+//var lista = ["uva", "pera", "maçã"]
+//lista.push("melão");
+
+//var fruta = {nome: "lucas", fruta: "maçã"}
+//console.log(fruta.nome);
+
+//console.log(lista)
+//console.log(lista.toString());
+//console.log(join(" - "));
+//console.log(lista[1]);
+//alert(lista[0]);
+
+//alert(nome + " tem " + idade + " anos");4
